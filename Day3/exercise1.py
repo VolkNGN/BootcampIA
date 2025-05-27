dda4fb687115dd609dda19178abd8573c77120c4
Sidney NGUYEN <Sidney.nguyen@live.fr>
2025-05-27 20:21:47 +0200

    Ajout de la vérification pair/impair : création de la fonction is_even(n) utilisant le modulo, et appel depuis une fonction check_even_or_odd(n)

--- Day3/exercise1.py
+++ Day3/exercise1.py
@@ -8,4 +8,18 @@ def print_factorial(n):
     result = factorial(n)
     print(f"The factorial of {n} is {result}")
     
-print_factorial(2)
+def is_even(n):
+    
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+
+def check_even_or_odd(n):
+    if is_even(n):
+        print(f"{n} is even.")
+    else:
+        print(f"{n} is odd.")
+
+check_even_or_odd(4)
+print_factorial(4)
