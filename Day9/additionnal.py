0a10e7e632df95d7c5da65c60719011b4f3bfe70
Sidney NGUYEN <Sidney.nguyen@live.fr>
2025-06-04 16:13:14 +0200

    ajout des exercices du jour 9

--- Day9/additionnal.py
+++ Day9/additionnal.py
@@ -1,16 +1,16 @@
 import numpy as np
 
 #Je veux Simuler des relevés de température (en degrés Celsius)
-np.random.seed(0)  # pour reproductibilité
-temperatures = np.random.normal(loc=45, scale=10, size=(10, 10))
+np.random.seed(0)
+temperatures = np.random.normal(loc=20, scale=10, size=(10, 10))
 print("Températures relevées :\n", temperatures)
 
 #Je définis un seuil d'alerte
-threshold = 50
+threshold = 30
 
 # j'utilise un masque binaire afin de convertir mes données en integer.
 mask = (temperatures > threshold).astype(int)
-print("\nMasque binaire (1 si > 50°C, sinon 0) :\n", mask)
+print("\nMasque binaire (1 si > 30°C, sinon 0) :\n", mask)
 
 #Je veux compter le nombre de températures supérieurs au seuil fixé
 count_above_threshold = np.sum(mask)
